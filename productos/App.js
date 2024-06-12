aa80c91c872a16c95fb47b12da8c3f4b40d4bb29
DIDIER K <kinderknechd@gmail.com>
2024-06-12 16:17:00 -0300

    v2

--- productos/App.js
+++ productos/App.js
@@ -13,7 +13,7 @@ const fijarProductos = () => {
         <div class="card-body text-center">
             <p class="card-text ">${producto.desc}</p>
             <p class="card-text ">$ ${producto.precio}</p>
-            <a href="#" class="btn btn-primary">AGREGAR AL CARRITO</a>
+            <a href="#" class="btn btn-primary agregar" id = "agregar">AGREGAR AL CARRITO</a>
         </div>
         </div>
         </div>`
@@ -24,3 +24,43 @@ const fijarProductos = () => {
 
 
 fijarProductos();
+
+
+// BUSCADOR DE PRODUCTOS
+
+function buscarProducto(event) {
+    event.preventDefault();
+
+    const busquedaInput = document.getElementById("busquedaInput");
+    const valorBusqueda = busquedaInput.value.toLowerCase();
+
+    const resultados = productos.filter(producto =>
+        producto.nombre.toLowerCase().includes(valorBusqueda)
+    );
+
+    mostrarResultados(resultados);
+}
+
+function mostrarResultados(resultados) {
+    const resultadosDiv = document.getElementById("resultadosBusqueda");
+    resultadosDiv.innerHTML = "";
+
+    if (resultados.length === 0) {
+        resultadosDiv.innerHTML = "No se encontraron resultados.";
+    } else {
+        resultados.forEach(producto => {
+            const card = `
+                <div class="card" style="width: 20rem;">
+                    <img src="${producto.img}" class="card-img-top" alt="Producto">
+                    <div class="card-body text-center">
+                        <p class="card-text">${producto.desc}</p>
+                        <p class="card-text">$ ${producto.precio}</p>
+                        <a href="#" class="btn btn-primary agregar" id = "agregar">AGREGAR AL CARRITO</a>
+                    </div>
+                </div>`;
+            const cardDiv = document.createElement("div");
+            cardDiv.innerHTML = card;
+            resultadosDiv.appendChild(cardDiv);
+        });
+    };
+};
--- productos/stok.js
+++ productos/stok.js
@@ -1,38 +1,91 @@
 const productos = [
     {
         id: 1,
-        nombre: "Buzo 1",
-        tipo: "buzo",
+        nombre: "Buzo",
+        tipo: "buzo overzice",
         desc: "Disponible en todos los talles, desde XS hasta XXL, para que encuentres el ajuste perfecto que se adapte a tu estilo y comodidad. ¡Atrévete a destacar con nuestro buzo oversize y conquista cualquier ocasión con un toque de tendencia!",
         precio: 50000,
         img: '../img/1.jpg',
         cantidad:1
     },
+
     {
-        id: 1,
-        nombre: "Buzo 1",
+        id: 2,
+        nombre: "Buzo",
         tipo: "buzo",
         desc: "Disponible en todos los talles, desde XS hasta XXL, para que encuentres el ajuste perfecto que se adapte a tu estilo y comodidad. ¡Atrévete a destacar con nuestro buzo oversize y conquista cualquier ocasión con un toque de tendencia!",
         precio: 50000,
         img: '../img/2.jpeg',
         cantidad:1
     },
+
     {
-        id: 1,
-        nombre: "Buzo 1",
+        id: 3,
+        nombre: "buzo",
         tipo: "buzo",
         desc: "Disponible en todos los talles, desde XS hasta XXL, para que encuentres el ajuste perfecto que se adapte a tu estilo y comodidad. ¡Atrévete a destacar con nuestro buzo oversize y conquista cualquier ocasión con un toque de tendencia!",
         precio: 50000,
         img: '../img/3.jpg',
         cantidad:1
     },
+
     {
-        id: 1,
-        nombre: "Buzo 1",
+        id: 4,
+        nombre: "Buzo",
         tipo: "buzo",
         desc: "Disponible en todos los talles, desde XS hasta XXL, para que encuentres el ajuste perfecto que se adapte a tu estilo y comodidad. ¡Atrévete a destacar con nuestro buzo oversize y conquista cualquier ocasión con un toque de tendencia!",
         precio: 50000,
-        img: '../img/4.jpg',
+        img: '../img/BUZO 1.webp',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "remera",
+        tipo: "buzo",
+        desc: "Disponible en todos los talles, desde XS hasta XXL, para que encuentres el ajuste perfecto que se adapte a tu estilo y comodidad. ¡Atrévete a destacar con nuestro buzo oversize y conquista cualquier ocasión con un toque de tendencia!",
+        precio: 50000,
+        img: '../img/5.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "remera",
+        tipo: "buzo",
+        desc: "Disponible en todos los talles, desde XS hasta XXL, para que encuentres el ajuste perfecto que se adapte a tu estilo y comodidad. ¡Atrévete a destacar con nuestro buzo oversize y conquista cualquier ocasión con un toque de tendencia!",
+        precio: 50000,
+        img: '../img/6.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 7,
+        nombre: "pantalon",
+        tipo: "pantalon",
+        desc: "Disponible en todos los talles, desde XS hasta XXL, para que encuentres el ajuste perfecto que se adapte a tu estilo y comodidad. ¡Atrévete a destacar con nuestro buzo oversize y conquista cualquier ocasión con un toque de tendencia!",
+        precio: 50000,
+        img: '../img/7.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 8,
+        nombre: "pantalon",
+        tipo: "pantalon",
+        desc: "Disponible en todos los talles, desde XS hasta XXL, para que encuentres el ajuste perfecto que se adapte a tu estilo y comodidad. ¡Atrévete a destacar con nuestro buzo oversize y conquista cualquier ocasión con un toque de tendencia!",
+        precio: 50000,
+        img: '../img/8.jpg',
+        cantidad:1
+    },
+    
+    {
+        id: 9,
+        nombre: "pantalon",
+        tipo: "pantalon",
+        desc: "Disponible en todos los talles, desde XS hasta XXL, para que encuentres el ajuste perfecto que se adapte a tu estilo y comodidad. ¡Atrévete a destacar con nuestro buzo oversize y conquista cualquier ocasión con un toque de tendencia!",
+        precio: 50000,
+        img: '../img/9.jpg',
         cantidad:1
     },
 ]
